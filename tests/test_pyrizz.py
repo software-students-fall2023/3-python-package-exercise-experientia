17dacf5f6c4d3f15ec8aa904fa0bc7856667a573
awesomeadi00 <adityapandhare44@gmail.com>
2023-11-03 10:30:11 -0400

    Added setup files

---
+++ tests/test_pyrizz.py
@@ -0,0 +1,13 @@
+import pytest
+
+class Tests: 
+    # An example of a pytest fixture - a function that can be used for setup and teardown before and after test functions are run.
+    @pytest.fixture
+    def example_fixture(self):
+        yield
+
+    # Test debugging... making sure that we can run a simple test that always passes.
+    def test_sanity_check(self, example_fixture):
+        expected = True 
+        actual = True 
+        assert actual == expected, "Expected True to be equal to True!"
