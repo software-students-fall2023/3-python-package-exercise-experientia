084cbb7c2a2115318d0028d4ed6841ad43bc6275
awesomeadi00 <adityapandhare44@gmail.com>
2023-11-08 16:29:06 -0500

    Tests...

--- tests/test_pyrizz.py
+++ tests/test_pyrizz.py
@@ -18,8 +18,8 @@ class Tests:
     # Tests if this returns an empty or not a proper category
     def test_get_lines_nonempty(self):
         actual = pyrizz.get_lines()
-        assert isinstance(actual, dict)
-        assert bool(actual)
+        assert isinstance(actual, list)
+        assert bool(actual) is True
 
 
     # Tests if this returns a string
@@ -53,7 +53,7 @@ class Tests:
 
     # Tests if the get random category line is a non-empty value
     def test_get_random_category_line_nonempty(self):
-        actual = pyrizz.get_random_category_line("romanctic_lines")
+        actual = pyrizz.get_random_category_line("romantic_lines")
         assert isinstance(actual, str)
         assert len(actual) > 0
 
@@ -65,7 +65,7 @@ class Tests:
 
     # Tests if the get random line is long enough
     def test_get_random_category_line_longenough(self):
-        actual = pyrizz.get_random_category_line("romanctic_lines")
+        actual = pyrizz.get_random_category_line("romantic_lines")
         assert isinstance(actual, str)
         assert len(actual) > 0
 
@@ -73,12 +73,15 @@ class Tests:
     # Tests if the input for ai line is empty
     def test_get_ai_line_empty(self):
         actual = pyrizz.get_ai_line("")
-        assert actual == "Please specify a category"
+        expected = "Please specify a category."
+        assert actual.strip() == expected.strip()
 
     # Tests if the input is way too long
     def test_get_ai_line_long(self):
         actual = pyrizz.get_ai_line("This is a very long category that is definitely more than 50 characters long.")
-        assert actual == "Please specify a category that is less than 50 characters"
+        expected = "Please specify a category that is less than 50 characters."
+        assert actual.strip() == expected.strip()
+
 
     # Tests if the input for ai line actually results in a string
     def test_get_ai_line_str(self):
@@ -89,7 +92,7 @@ class Tests:
     # Tests if the rate line is empty
     def test_rate_line_empty(self):
         actual = pyrizz.rate_line("")
-        assert actual == "No pickup line? You gotta use our other features before you come here buddy"
+        assert actual == "No pickup line? You gotta use our other features before you come here buddy."
 
     # Tests if the rate line function follows a specific format
     def test_rate_line_format(self):
@@ -98,29 +101,17 @@ class Tests:
 
 
     def test_add_user_line_invalid_template_number(self, capsys):
-        with pytest.raises(SystemExit):
-            with pytest.raises(SystemExit):
-                pyrizz.add_user_line()
-        captured = capsys.readouterr()
-        assert "Template number out of range. Please choose between 1 and 20." in captured.out
-
-    def test_add_user_line_not_enough_words(self, capsys):
-        with pytest.raises(SystemExit):
-            with pytest.raises(SystemExit):
-                pyrizz.add_user_line()
-        captured = capsys.readouterr()
-        assert "Not enough words provided for the placeholders." in captured.out
+        with patch('builtins.input', side_effect=["21", "exit"]):
+            pyrizz.add_user_line()
+            captured = capsys.readouterr()
+            assert "Template number out of range. Please choose between 1 and 20." in captured.out
 
     def test_is_line_valid_length(self):
         long_line = "x" * 141
         assert not pyrizz.is_line_valid(long_line)
 
-    def test_is_offensive_detection(self, monkeypatch):
-        monkeypatch.setattr(pyrizz, "is_offensive", Mock(return_value=True))
-        offensive_line = "An example offensive line"
-        assert pyrizz.is_offensive(offensive_line)
-
-    def test_is_offensive_openai_error_handling(self, monkeypatch):
-        monkeypatch.setattr(pyrizz, "is_offensive", Mock(side_effect=Exception("Test error")))
+    def test_is_offensive_openai_error_handling(self):
         line = "This should cause an OpenAIError"
-        assert not pyrizz.is_offensive(line)
+        assert pyrizz.is_offensive(line) is False, "Expected to return False when an OpenAIError occurs."
+
+
